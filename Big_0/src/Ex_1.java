f6813c7e202cb52fdbfaa76f87d79eea78376980
tommy-acton <tommy-acton@hotmail.co.uk>
2018-04-17 19:37:02 +0100

    making my first push

---
+++ Big_0/src/Ex_1.java
@@ -0,0 +1,25 @@
+public class Ex_1 {
+
+    /* Exercise 1 */
+    public  static int sum(int n) {
+        if (n <= 0) {
+            return 0;
+        } else {
+            return n + sum(n -1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(4));
+
+        /*
+        * sum(4)
+        *   --> sum(3)
+        *       --> sum(2)
+        *           --> sum(1)
+        *               --> sum(0)
+        * */
+        /**
+         * Each of these calls is added to the call stack and takes up actual memory
+         * */
+    }
+}
