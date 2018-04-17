86920a7aab409f9b53b4684df55fc25e20d46a8e
tommy-acton <tommy-acton@hotmail.co.uk>
2018-04-17 21:07:38 +0100

    Big 0- ex1,2 - Done

--- Big_0/src/Ex_2.java
+++ Big_0/src/Ex_2.java
@@ -1,2 +1,25 @@
 public class Ex_2 {
+
+    public static int pairSumSequence(int n){
+
+            int sum = 0; // Creating a local variable here and setting it to 0;
+
+            for(int i= 0; i < n; i++){
+                sum += pairSum(i, i +1);
+            }
+        return sum;
+    }
+
+    public static int pairSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main (String[] args) {
+        //System.out.println("Hello I Work!");
+        System.out.println(pairSumSequence(5));
+    }
+
+    /**
+     * There will be roughly O(n) calls to pairSum. However, those calls do not exist simultaneously
+     * */
 }
